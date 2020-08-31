bcb04d2c9071cd4cb28ef7bbfb8e6eb6c500d762
embraman10@gmail.com <embraman10@gmail.com>
2020-08-31 12:32:28 -0500

    Done.

--- src/components/ToyContainer.jsx
+++ src/components/ToyContainer.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ToyCard from './ToyCard'
 
-const ToyContainer = () => {
+const ToyContainer = (props) => {
+  console.log(props)
   return(
     <div id="toy-collection">
-      {/* Render the collection of ToyCards */}
+      {props.toys.map(toyObj => {
+        return <ToyCard toy={toyObj} destroyToy={props.destroyToy} updateLikes={props.updateLikes}/>
+      })}
     </div>
   );
 }
--- src/components/ToyForm.jsx
+++ src/components/ToyForm.jsx
@@ -2,14 +2,59 @@ import React, { Component } from 'react';
 
 class ToyForm extends Component {
 
+  state = { 
+    name: '',
+    image: ''
+
+  }
+
+
+  handleChangeName = (e) => {
+    console.log(e.target.value)
+    this.setState({
+      name: e.target.value
+    })
+  }
+
+  handleChangeImage = (e) => {
+    console.log(e.target.value)
+    this.setState({
+      image: e.target.value
+    })
+  }
+
+  handleSubmit = (e) =>{
+    e.preventDefault()
+    const newToy = { 
+      ...this.state,
+    like: 0}
+
+    
+    const reqObj = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newToy)
+    }
+
+    fetch('http://localhost:3000/toys', reqObj)
+    .then(resp => resp.json())
+    .then(newToy => {
+      this.props.addToy(newToy)
+    })
+
+  }
+
+
   render() {
     return (
       <div className="container">
-        <form className="add-toy-form">
+        <form onSubmit={this.handleSubmit} className="add-toy-form">
           <h3>Create a toy!</h3>
-          <input type="text" name="name" placeholder="Enter a toy's name..." className="input-text"/>
+          <input onChange={this.handleChangeName} value={this.state.name} type="text" name="name" placeholder="Enter a toy's name..." className="input-text"/>
           <br/>
-          <input type="text" name="image" placeholder="Enter a toy's image URL..." className="input-text"/>
+          <input onChange={this.handleChangeImage} value={this.state.image} type="text" name="image" placeholder="Enter a toy's image URL..." className="input-text"/>
           <br/>
           <input type="submit" name="submit" value="Create New Toy" className="submit"/>
         </form>
