4c617194ecec983ec42aa1f9093cfe3f0c5d2aa4
Eve Reichmann <eve.reichmann@gmail.com>
2020-08-30 21:53:03 -0500

    Done.

--- src/components/ToyCard.jsx
+++ src/components/ToyCard.jsx
@@ -2,14 +2,38 @@ import React, { Component } from 'react';
 
 class ToyCard extends Component {
 
+  donateClick = () => {
+    this.props.deleteToy(this.props.toy.id)
+  }
+
+  likeClick = () => {
+    fetch('http://localhost:3000/toys/' + this.props.toy.id , {
+      method: 'PATCH',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ 
+        likes: this.props.toy.id + 1
+       })
+    })
+      .then(resp => resp.json())
+      .then(toyData => {
+        this.props.likeAToy(this.props.toy)
+      })
+
+
+  }
+
   render() {
+    const { name, image, likes } = this.props.toy
     return (
       <div className="card">
-        <h2>{'' /* Toy's Name */}</h2>
-        <img src={'' /* Toy's Image */} alt={/* Toy's Name */} className="toy-avatar" />
-        <p>{'' /* Toy's Likes */} Likes </p>
-        <button className="like-btn">Like {'<3'}</button>
-        <button className="del-btn">Donate to GoodWill</button>
+        <h2>{ name }</h2>
+        <img src={image} alt={name} className="toy-avatar" />
+        <p>{likes} Likes </p>
+        <button className="like-btn" onClick={this.likeClick}>Like {'<3'}</button>
+        <button className="del-btn" onClick={this.donateClick}>Donate to GoodWill</button>
       </div>
     );
   }
--- src/components/ToyForm.jsx
+++ src/components/ToyForm.jsx
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
 
 class ToyForm extends Component {
+  // constructor(){
+  //   super()
+  //   this.state = {
+      
+  //     newToy: []
+  //   }
+  // }
 
   render() {
     return (
       <div className="container">
-        <form className="add-toy-form">
+        <form className="add-toy-form" onSubmit={this.props.handleSubmit}>
           <h3>Create a toy!</h3>
-          <input type="text" name="name" placeholder="Enter a toy's name..." className="input-text"/>
+          <input type="text" name="name" placeholder="Enter a toy's name..." className="input-text" onChange={this.props.handleName} value={this.props.name}/>
           <br/>
-          <input type="text" name="image" placeholder="Enter a toy's image URL..." className="input-text"/>
+          <input type="text" name="image" placeholder="Enter a toy's image URL..." className="input-text" onChange={this.props.handleImage} value={this.props.image}/>
           <br/>
           <input type="submit" name="submit" value="Create New Toy" className="submit"/>
         </form>
