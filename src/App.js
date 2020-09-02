e8b4648dadc3de89d972c093c342cd5d656c1e16
foxten <tenaysiafox@gmail.com>
2020-09-01 20:42:31 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -9,9 +9,22 @@ import data from './data'
 
 
 class App extends React.Component{
+  constructor(){
+      super()
+      this.state = {
+        display: false,
+        allToys: []
+      }
+  }
 
-  state = {
-    display: false
+  componentDidMount(){
+    fetch(`http://localhost:3000/toys`)
+      .then(response => response.json())
+        .then(toyData => {
+            this.setState({
+              allToys: toyData
+      })
+    })
   }
 
   handleClick = () => {
@@ -21,20 +34,67 @@ class App extends React.Component{
     })
   }
 
+  handleLike = (num, id) =>{
+    console.log(num, id)
+    const reqObj={
+      method: 'PATCH', 
+      headers: {'Content-Type': 'application/json'},
+      body:JSON.stringify({likes: num})
+    }
+    fetch(`http://localhost:3000/toys/${id}`, reqObj)
+      .then(resp => resp.json())
+        .then(updatedToy => {
+          let allToys = this.state.allToys
+          let originalToy = allToys.find(toy => toy.id === updatedToy.id)
+          allToys.splice(allToys.indexOf(originalToy), 1, updatedToy)
+          this.setState({
+            allToys: allToys
+          })
+        })
+  }
+
+  handleDelete = (id) =>{
+    // console.log('gotta delete me')
+    const reqObj={
+      method: 'DELETE'
+    }
+    fetch(`http://localhost:3000/toys/${id}`, reqObj)
+      .then(resp => resp.json())
+        .then(updatedToy => {
+          let allToys = this.state.allToys
+          let originalToy = allToys.find(toy => toy.id === id)
+          allToys.splice(allToys.indexOf(originalToy), 1)
+          this.setState({
+            allToys: allToys
+          })
+        })
+  }
+
+  handleSubmit = (info) =>{
+    const reqObj = {
+      method: 'POST',
+      headers:{'Content-Type': 'application/json'},
+      body: JSON.stringify(info)
+    }
+
+    fetch(`http://localhost:3000/toys`, reqObj)
+      .then(response => response.json())
+        .then(newToy => {
+          this.setState({
+            allToys: [...this.state.allToys, newToy]
+          })
+        })
+  }
+
   render(){
     return (
       <>
         <Header/>
-        { this.state.display
-            ?
-          <ToyForm/>
-            :
-          null
-        }
+        {this.state.display? <ToyForm handleSubmit={this.handleSubmit}/> : null}
         <div className="buttonContainer">
           <button onClick={this.handleClick}> Add a Toy </button>
         </div>
-        <ToyContainer/>
+        <ToyContainer handleDelete={this.handleDelete} handleLiked={this.handleLike} toys={this.state.allToys}/>
       </>
     );
   }
